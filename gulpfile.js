f440e87c7efdfb14b4958ac375526e26e3552bac
crazyyy <crazyyy@gmail.com>
2017-05-18 17:01:25 +0300

    gulped

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 /* if work with html set TRUE, else - FALSE */
-var htmlOWp = true,
+var htmlOWp = false,
   // set wordpress template folder
   wpThemeName = 'wp-photographer',
   // and set wordpress domain
---
+++ wordpress/wp-content/themes/wp-photographer/js/wp-fix.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(m){m("form#commentform").prepend(m("form#commentform p.comment-form-comment"))});
+//# sourceMappingURL=maps/wp-fix.js.map
